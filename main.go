171a099454246142495fd8974fb28a137a8f7000
Bensoncasey7 <bensoncasey7@gmail.com>
2023-12-01 14:11:59 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"advent-of-code-2023/solutions"
+)
+
+func main() {
+	solutions.Day1()
+}
---
+++ solutions/Day1.go
@@ -0,0 +1,8 @@
+package solutions
+
+import "fmt"
+
+func Day1() {
+	// Your solution for Day 1
+	fmt.Println("Day 1 Solution")
+}
